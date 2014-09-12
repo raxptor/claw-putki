b445147fb1b0c377821858ed69a865904fc988e5
Dan Nilsson <raxptor@gmail.com>
2014-09-12 14:28:35 +0200

    - fix wrong path

--- src/game/staticdata.cpp
+++ src/game/staticdata.cpp
@@ -3,16 +3,23 @@
 #include <putki/pkgmgr.h>
 #include <putki/pkgloader.h>
 
+#include <claw/log.h>
+
 namespace game
 {
 	namespace
 	{
 		putki::pkgmgr::loaded_package *s_pkg = 0;
+        const char *s_static_package_name = "static.pkg";
 	}
 
 	void load_static_package()
 	{
-		s_pkg = putki::pkgloader::from_file("static.pkg");
+		s_pkg = putki::pkgloader::from_file(s_static_package_name);
+        if (!s_pkg)
+        {
+            CLAW_ERROR("Failed to load required package[" << s_static_package_name << "]");
+        }
 	}
 
 	outki::globalsettings *get_global_settings()
