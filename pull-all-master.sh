bf74713ca9f4b56dc5d8880328200af1d88088d3
Dan Nilsson <dan@datacraft.se>
2014-10-06 20:03:21 +0200

    - More script fixes

--- pull-all-master.sh
+++ pull-all-master.sh
@@ -4,6 +4,7 @@ git checkout master
 git pull --rebase origin master
 git submodule sync
 git submodule update
+git submodule foreach git fetch origin
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
 git submodule foreach git status -s
